be759c2d5e046336f8505750d11ce75b86a9eb54
Ben Webb <ben@salilab.org>
2022-07-08 00:21:32 -0700

    Improve documentation

--- modelcif/descriptor.py
+++ modelcif/descriptor.py
@@ -9,7 +9,16 @@
 
 
 class Descriptor(object):
-    """Base class for all descriptors"""
+    """Base class for all descriptors.
+       This class is generally not used directly; instead, a subclass
+       such as :class:`IUPACName` or :class:`InChI` is employed.
+
+       :param str value: The actual name or identifier describing
+              the chemistry.
+       :param str details: Additional details about this descriptor.
+       :param software: The software used to generate the descriptor, if any.
+       :type software: :class:`modelcif.Software`
+    """
 
     def __init__(self, value, details=None, software=None):
         self.value, self.details = value, details
@@ -21,13 +30,13 @@ class Descriptor(object):
 
 class CanonicalSMILES(Descriptor):
     """Simplified Molecular-Input Line-Entry System (SMILES) computed from
-       chemical structure devoid of isotopic and stereochemical information"""
+       chemical structure devoid of isotopic and stereochemical information."""
     type = 'Canonical SMILES'
 
 
 class IsomericSMILES(Descriptor):
     """Simplified Molecular-Input Line-Entry System (SMILES) computed from
-       chemical structure containing isotopic and stereochemical information
+       chemical structure containing isotopic and stereochemical information.
 
        SMILES written with isotopic and chiral specifications are collectively
        known as isomeric SMILES."""
@@ -36,24 +45,24 @@ class IsomericSMILES(Descriptor):
 
 class IUPACName(Descriptor):
     """Chemical name computed from chemical structure that uses International
-       Union of Pure and Applied Chemistry (IUPAC) nomenclature standards"""
+       Union of Pure and Applied Chemistry (IUPAC) nomenclature standards."""
     type = 'IUPAC Name'
 
 
 class InChI(Descriptor):
     """International Chemical Identifier (InChI) computed from chemical
        structure using the International Union of Pure and Applied Chemistry
-       (IUPAC) standard"""
+       (IUPAC) standard."""
     type = 'InChI'
 
 
 class InChIKey(Descriptor):
     """International Chemical Identifier hash (InChIKey) computed from
        chemical structure using the International Union of Pure and Applied
-       Chemistry (IUPAC) standard"""
+       Chemistry (IUPAC) standard."""
     type = 'InChI Key'
 
 
 class PubChemCID(Descriptor):
-    """PubChem Compound ID"""
+    """PubChem Compound ID."""
     type = 'PubChem CID'
