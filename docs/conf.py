91e0a236480c994955a24e7869f5e8ddf04d4646
Ben Webb <ben@salilab.org>
2023-01-09 11:56:39 -0800

    Update copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Python-ModelCIF'
-copyright = u'2021-2022, Benjamin Webb'
+copyright = u'2021-2023, Benjamin Webb'
 author = u'Benjamin Webb'
 
 # The version info for the project you're documenting, acts as replacement for
