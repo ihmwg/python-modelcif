81299978ccc703916e0d19c675f000dc75414b4b
Ben Webb <ben@salilab.org>
2023-07-31 12:59:08 -0700

    We may not have BinaryCIF support at test time

--- modelcif/test.py
+++ modelcif/test.py
@@ -14,24 +14,16 @@ class Tests(unittest.TestCase):
         entity_b = modelcif.Entity('AAAAAA', description='Subunit B')
         system.entities.extend((entity_a, entity_b))
 
-        # Test output in mmCIF and BinaryCIF formats
+        # Test output in mmCIF format
         with open('output.cif', 'w') as fh:
             modelcif.dumper.write(fh, [system])
 
-        with open('output.bcif', 'wb') as fh:
-            modelcif.dumper.write(fh, [system], format='BCIF')
-
-        # Make sure we can read back the files
+        # Make sure we can read back the file
         with open('output.cif') as fh:
             sys2, = modelcif.reader.read(fh)
         self.assertEqual(sys2.title, 'test system')
 
-        with open('output.bcif', 'rb') as fh:
-            sys2, = modelcif.reader.read(fh, format='BCIF')
-        self.assertEqual(sys2.title, 'test system')
-
         os.unlink('output.cif')
-        os.unlink('output.bcif')
 
 
 if __name__ == '__main__':
