d4a56caad89df278e536d5faab9a84f2e7c3284d
Ben Webb <ben@salilab.org>
2023-10-02 23:16:06 -0700

    Remove duplicate COPR; we don't build for Homebrew

--- make-release.sh
+++ make-release.sh
@@ -12,11 +12,10 @@
 # - Make release on GitHub
 # - Upload the release tarball from
 #   https://github.com/ihmwg/python-modelcif/releases to Zenodo as a new release
-# - Build SRPM and upload to COPR
 # - Make sure there are no extraneous .py files (setup.py will include them
 #   in the pypi package)
 
 VERSION=$(python3 setup.py --version)
 python3 setup.py sdist
 echo "Now use 'twine upload dist/modelcif-${VERSION}.tar.gz' to publish the release on PyPi"
-echo "Then, update the conda-forge, COPR, and Homebrew packages to match."
+echo "Then, update the conda-forge and COPR packages to match."
