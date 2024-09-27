44ec942f1baf8c0c44aedef1a0462ae43b63383c
Ben Webb <ben@salilab.org>
2024-09-27 12:58:57 -0700

    RPM specfile also should get required IHM version

--- make-release.sh
+++ make-release.sh
@@ -5,7 +5,7 @@
 # - Run util/validate-outputs.py to make sure all example outputs validate
 #   (cd util; PYTHONPATH=.. python3 ./validate-outputs.py)
 # - If we need a newer python-ihm, update the version requirement in
-#   requirements.txt, setup.py, and README.md.
+#   requirements.txt, setup.py, util/python-modelcif.spec, and README.md.
 # - Update ChangeLog.rst and util/python-modelcif.spec with the release number
 # - Update release number in modelcif/__init__.py and setup.py
 # - Commit, tag, and push
